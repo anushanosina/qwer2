a1a58157a02e3214f9358dc33210d50a59ac460b
anushanosina <nosinaanusha12@gmail.com>
2024-11-10 05:15:51 -0800

    Add files via upload

---
+++ DIVISION.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.division;
+
+/**
+ *
+ * @author Dell
+ */
+public class DIVISION {
+
+    public static void main(String[] args) {
+       int a=4;
+       int b=2;
+       int c=a/b;
+       System.out.println("the division is:"+c);
+       
+    }
+}
